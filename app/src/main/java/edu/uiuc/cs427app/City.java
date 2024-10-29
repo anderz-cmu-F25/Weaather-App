7d713775cda5a20e0c363698492f329d0aa57f6d
Ryan Forester <ryanef2@illinois.edu>
2024-10-28 22:55:53 -0500

    City class

--- app/src/main/java/edu/uiuc/cs427app/City.java
+++ app/src/main/java/edu/uiuc/cs427app/City.java
@@ -1,4 +1,26 @@
 package edu.uiuc.cs427app;
 
 public class City {
+
+    private String name;
+    // Latitude
+    private double latitude;
+    // Longitude
+    private double longitude;
+
+    public City(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
 }
