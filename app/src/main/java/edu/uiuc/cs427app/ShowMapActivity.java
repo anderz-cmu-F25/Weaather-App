41ccad83966fe25e645e7a0e9a32d0a2e29357ad
W1ndfan <shijie.fan@outlook.com>
2024-11-06 21:14:45 -0500

    Use populated city coordinates

--- app/src/main/java/edu/uiuc/cs427app/ShowMapActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/ShowMapActivity.java
@@ -13,11 +13,18 @@ import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class ShowMapActivity extends AppCompatActivity implements View.OnClickListener {
     // Define SharedPreferences constants for saving user settings
@@ -40,16 +47,18 @@ public class ShowMapActivity extends AppCompatActivity implements View.OnClickLi
         MainActivity.applyBackgroundColor(backgroundColor, wxLayout);
 
         String cityName = getIntent().getStringExtra("city");
+        String currentUsername = getIntent().getStringExtra("username");
 
-        MapService task = new MapService();
-        String apiKey = getApiKey();
-        City city;
-        try {
-            city = task.execute(cityName, apiKey).get();
-        } catch (Exception e) {
-            e.printStackTrace();
-            city = new City("Invalid", Double.MIN_VALUE, Double.MIN_VALUE);
+        ArrayList<City> cityList = new ArrayList<>();
+        Gson gson = new Gson();
+        String json = preferences.getString(currentUsername + "_cities", "");
+        if (!json.isEmpty()) {
+            Type type = new TypeToken<ArrayList<City>>(){}.getType();
+            cityList = gson.fromJson(json, type);
         }
+
+        List<City> foundCityList = cityList.stream().filter(city -> city.getName().equals(cityName)).collect(Collectors.toList());;
+        City city = foundCityList.isEmpty() ? new City("Invalid", Double.MIN_VALUE, Double.MIN_VALUE) : foundCityList.get(0);
 
         TextView cityNameView = findViewById(R.id.cityName);
         TextView latLongView = findViewById(R.id.latLong);
@@ -69,7 +78,7 @@ public class ShowMapActivity extends AppCompatActivity implements View.OnClickLi
         // Generate the map URL using the city's name
         String mapUrl = null;
         try {
-            mapUrl = String.format("https://www.google.com/maps/embed/v1/place?key=%s&q=%s", apiKey, URLEncoder.encode(cityName, StandardCharsets.UTF_8.name()));
+            mapUrl = String.format("https://www.google.com/maps/embed/v1/place?key=%s&q=%s", getApiKey(), URLEncoder.encode(cityName, StandardCharsets.UTF_8.name()));
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
